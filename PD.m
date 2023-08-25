212e9f3d0a126da1a88845629743d6859b03cc08
qizixinge <128884645+qizixinge@users.noreply.github.com>
2023-08-25 22:09:39 +0800

    PD.m

--- PD.m
+++ PD.m
@@ -1,4 +1,4 @@
-function [fim] = PD(nim, gim, noise_map, x1)
+function [fim] = PD(nim, gim, noise_map, T)
 %noise map can be obtained by a noise estimator
 %noise map should have the same size as nim
 NNim1 = padarray(nim,[5, 5, 5],'symmetric');
@@ -7,10 +7,8 @@ Noise_map=padarray(noise_map,[5, 5, 5],'symmetric');
 miu = imboxfilt3( GGim  , 'padding' , 'symmetric' ) ;%3D mean matrix of ggim
 PRINLMfim = RI_NLM(GGim, NNim1, Noise_map, miu,0.4);%~5min
 
-[fim,~]=NLPCA(PRINLMfim,x1(1), x1(2), x1(3));
-%when x1(1) * x1(2) is constant, the performance of PD remains unchanged
-%we could let x1(3) = x1(1) * x1(2) according to the discussion in the
-%paper
+[fim,~]=NLPCA(PRINLMfim, 1, T, T);
+%when tau * beta is constant, the performance of PD remains unchanged
 yichang=find(isnan(fim));
 fim(yichang)=PRINLMfim(yichang);
 
