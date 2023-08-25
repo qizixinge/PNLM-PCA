2c5e40456e4b48955112fd550099b91bc8148415
qizixinge <128884645+qizixinge@users.noreply.github.com>
2023-08-25 22:05:27 +0800

    PD.m

--- PD.m
+++ PD.m
@@ -9,6 +9,8 @@ PRINLMfim = RI_NLM(GGim, NNim1, Noise_map, miu,0.4);%~5min
 
 [fim,~]=NLPCA(PRINLMfim,x1(1), x1(2), x1(3));
 %when x1(1) * x1(2) is constant, the performance of PD remains unchanged
+%we could let x1(3) = x1(1) * x1(2) according to the discussion in the
+%paper
 yichang=find(isnan(fim));
 fim(yichang)=PRINLMfim(yichang);
 
